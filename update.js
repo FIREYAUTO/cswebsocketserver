e7e5c56dc442671940a99302a2898de38d62bc50
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-10-05 20:22:52 -0700

    Create update.js

---
+++ update.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const path = require("path");
+const HTTPModule = require("https");
+const QueryString = require("querystring");
+
+const HTTP = {
+  Get:async function(Host,Path){
+    let Results = await new Promise(res=>{
+      let req = HTTPModule.request({
+        hostname:Host,
+        path:Path,
+        method:"GET",
+      },r=>{
+		let rawData="";
+		r.on("data",chunk=>{rawData+=chunk});
+		r.on("end",()=>{
+			res(rawData);
+		});
+      });
+      req.end();
+    }).then(a=>{
+      return a.toString();
+    });
+    return Results;
+  },
+  URLEncode:function(Data){
+    return QueryString.stringify(Data);
+  },
+};
+
+const files = ["site/customize.css","site/index.html","site/site.js","site/style.css","update.js"];
+const updateUrl = "/cswebsocketserver/";
+
+(async()=>{
+	const D = Date.now();
+	console.log("\x1b[34m","Update Script Starting",'\x1b[0m')
+	try {
+		let fl = files.length;
+		for(let k in files){
+			k=+k;
+			let name = files[k]
+			let contents = await HTTP.Get("fireyauto.github.io",updateUrl+name);
+			console.log(`Updating "${name}" [${k+1}/${fl}]`);
+			fs.writeFileSync(path.join(__dirname,name),contents);
+			console.log("\x1b[32m",`Done Updating "${name}" [${k+1}/${fl}]`,'\x1b[0m');
+		}
+	}catch(e){
+		console.log("\x1b[31m",`This script has errored! Contact the developer with a screenshot of this message\n${e.toString()}`,'\x1b[0m');
+	}
+	console.log("\x1b[34m",`Update Script Finished; took ${Date.now()-D}ms`,'\x1b[0m')
+})()

86d9e6eae46311e7148eaf48c9e88f6bbce086f9
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-10-05 21:09:45 -0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,1 +1,1 @@
-npm init --yes && npm i -D electron@latest && npm i ws && npm run update && npm run update
+npm init --yes && npm i -D electron@latest && npm i ws && node update.js && node update.js

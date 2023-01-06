001a847dbf0fadf095bcdd7ac636759be97fd126
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2023-01-05 19:47:28 -0800

    Update site.js

--- site/site.js
+++ site/site.js
@@ -1,3 +1,9 @@
+//{{ Settings }}\\
+
+const St = {
+	WSMessages: true,
+};
+
 //{{ Websocket Functions }}\\
 
 async function NewSocket(URL){
@@ -130,6 +136,7 @@ async function Start(){
 	});
 
 	IPC.on("RobloxClientMessage",(Event,Data)=>{
+		if(!St.WSMessages)return;
 		Data=JSON.parse(Data);
 		BSay(`${Data.Name}: ${Data.Message}`);
 	})
@@ -162,6 +169,163 @@ function ColorWord(t,c){
 	return `<span style="color:${c}">${t}</span>`;
 }
 
+// ++ -- ++ {{ COMMAND SYSTEM }} ++ -- ++ \\
+
+// Settings
+
+const Settings = {
+	Prefix: ".",
+	Splitter: " ",
+	ParameterTypes: {
+		String: function(self){
+			return String(self.Token);
+		},
+		Integer: function(self){
+			return parseInt(self.Token);
+		},
+		Float: function(self){
+			return parseFloat(self.Token);
+		},
+		Boolean: function(self){
+			return ["on","1","true","yes"].includes(self.Token.toLowerCase())?true:false;
+		},
+		LongString: function(self){
+			let String = [];
+			while(!self.IsEnd())
+				String.push(self.Token),self.Next();
+			return String.join(Settings.Splitter);
+		},
+	},
+	Commands: [],
+};
+
+// Classes
+
+class Stack {
+	constructor(Arguments){
+		this.Tokens = Arguments,
+		this.Position = 0,
+		this.Token = Arguments[0];
+	}
+	Next(Amount=1){
+		this.Position+=Amount;
+		this.Token=this.Tokens[this.Position];
+		return this.Token;
+	}
+	IsEnd(){
+		return this.Position >= this.Tokens.length;
+	}
+}
+
+class Command {
+	constructor(Options={}){
+		this.Names = Options.Names||[],
+		this.Description = Options.Description||"",
+		this.Call = Options.Call,
+		this.Parameters = Options.Parameters||[];
+	}
+	Fire(Parameters=[]){
+		let P = this.Parameters,
+			S = new Stack(Parameters),
+			PS = [];
+		for(let PN of P)
+			if(S.IsEnd())break;
+			else PS.push(Settings.ParameterTypes[PN.split(":")[1]](S)),S.Next();
+		return this.Call(...PS);
+	}
+}
+
+class CommandResult {
+	constructor(Success=false,Result){
+		this.Success=Success,this.Result=Result;
+	}
+	toString(){
+		return `${this.Success}, ${this.Result}`;
+	}
+}
+
+// Command System
+
+const CommandSystem = {
+	Add: function(Value){
+		if(Value instanceof Command)Settings.Commands.push(Value);
+	},
+	Run: function(Text){
+		const Message = Text.split(Settings.Splitter);
+		if(!Message[0].startsWith(Settings.Prefix))return new CommandResult(false,"No Prefix");
+		Message[0]=Message[0].substring(Settings.Prefix.length,Message[0].length).toLowerCase();
+		for(let Command of Settings.Commands){
+			if(Command.Names.includes(Message[0])){
+				let Success=false,Result="";
+				Message.shift();
+				try{
+					Result = Command.Fire(Message),
+					Success = true;
+				}catch(E){
+					Result = E;
+				}
+				return new CommandResult(Success,Result);
+			}
+		}
+	},
+}
+
+// Commands
+
+CommandSystem.Add(new Command({
+	Names: ["log"],
+	Parameters: ["Text:LongString"],
+	Description: "Logs the given text to the output",
+	Call: function(Text){
+		Say({
+			Text:Text,
+			TextColor:"#bfbfbf",
+		});
+	},
+}));
+
+CommandSystem.Add(new Command({
+	Names: ["cmds","commands","help"],
+	Parameters: [],
+	Description: "Prints out all commands",
+	Call: function(){
+		for(let Command of Settings.Commands){
+			let Names = "";
+			if(Command.Names.length>1) Names="&lt;"+Command.Names.join(", ")+"&gt;";
+			else Names=Command.Names[0];
+			let Parameters = [];
+			for(let PN of Command.Parameters){
+				let P = PN.split(":");
+				Parameters.push(`[${P[0]}: ${P[1]}]`);
+			}
+			Parameters = Parameters.join(Settings.Splitter);
+			Info(`${Settings.Prefix}${Names}${Settings.Splitter}${Parameters} (${Command.Description})`);
+		}
+	},
+}));
+
+CommandSystem.Add(new Command({
+	Names: ["wsmessage","wsmsg"],
+	Parameters: ["Enabled:Boolean"],
+	Description: "Toggles WSMessages",
+	Call: function(Enabled){
+		let N = "WSMessages";
+		St[N]=Enabled
+		Info(`${N} is now ${Enabled?"on":"off"}`);
+	},
+}));
+
+CommandSystem.Add(new Command({
+	Names: ["send"],
+	Parameters: ["Text:LongString"],
+	Description: "Sends the given text to the connected Roblox Client",
+	Call: function(Text){
+		IPC.send("RobloxClientSend",Text);
+	},
+}));
+
+//
+
 //{{ Input }}\\
 
 var Blurred = true
@@ -181,7 +345,13 @@ Bar.addEventListener("keyup",event=>{
 			Text:Bar.value,
 			TextColor:"#bfbfbf",
 		});
-		IPC.send("RobloxClientSend",Bar.value);
+		let CR = CommandSystem.Run(Bar.value);
+		if(!CR.Success){
+			Say({
+				Text:CR.Result,
+				TextColor:"#ff1c4a"
+			})
+		}
 		Bar.value = "";
 	} else if (event.keyCode == 27){
 		Bar.value = "";
